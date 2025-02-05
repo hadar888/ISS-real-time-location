b47b06c69ea60a6d517d8f40fd1b534e478311e2
הדר שושן <hadarhadar888@gmail.com>
2025-02-05 23:51:59 +0200

    adding useEffect in client side and cors in the server side

---
+++ client/src/AppConfig.ts
@@ -0,0 +1,5 @@
+const appConfig = {
+    serverUrl: 'http://localhost:4000',
+}
+
+export default appConfig;
--- client/src/components/MapComponent.tsx
+++ client/src/components/MapComponent.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import { LatLngExpression } from 'leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { LatLngExpression } from 'leaflet'; 
+import appConfig from '../AppConfig';
+
+const intervalDuration = 15000;
 
 const MapComponent = () => {
-  const position: LatLngExpression = [0, 0];
+  const [currentLocation, setCurrentLocation] = useState<LatLngExpression | undefined>();
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetch(appConfig.serverUrl)
+        .then((response) => response.json())
+        .then((data) => {
+          setCurrentLocation([data.iss_position.latitude, data.iss_position.longitude]);
+        })
+        .catch((error) => {
+          console.error('Error fetching ISS location:', error);
+        });
+    }, intervalDuration);
+
+    return () => clearInterval(intervalId);
+  }, [])
 
   return (
-    <MapContainer center={position} zoom={3} style={{ height: '100vh' }}>
+    <MapContainer center={[0, 0]} zoom={3} style={{ height: '100vh' }}>
       <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position} />
+      {currentLocation && <Marker position={currentLocation} />}
     </MapContainer>
   );
 };
