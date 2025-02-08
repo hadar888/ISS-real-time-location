02614dac5d71786b869bc203ce42ee3d97b8f3d8
הדר שושן <hadarhadar888@gmail.com>
2025-02-08 21:33:58 +0200

    adding cache to the server

---
+++ server/src/appConfig.ts
@@ -0,0 +1,7 @@
+const appConfig = {
+    port: 4000,
+    clientUrl: 'http://localhost:3000',
+    apiUrl: 'http://api.open-notify.org/iss-now.json'
+}
+
+export default appConfig;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,23 +1,36 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
 import cors from 'cors';
+import appConfig from './appConfig';
+import { IssLocation } from './modals';
 
 const app = express();
-const port = 4000;
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST'],
+  origin: appConfig.clientUrl,
+  methods: ['GET'],
   allowedHeaders: ['Content-Type'],
 };
 
 app.use(cors(corsOptions));
 
+let cache: IssLocation = { data: null, timestamp: 0 };
+const CACHE_DURATION = 1000;
+
 app.get('/', async (req: Request, res: Response) => {
   console.log('trying to get ISS current location...');
+
+  const now = Date.now();
+
   try {
-    const response = await axios.get('http://api.open-notify.org/iss-now.json');
-    res.json(response.data);
+    if (cache.data && now - cache.timestamp < CACHE_DURATION) {
+      res.json(cache.data);
+    }
+    else {
+      const response = await axios.get(appConfig.apiUrl);
+      cache = { data: response.data.iss_position, timestamp: now };
+      res.json(response.data);
+    }
   } catch (error) {
     console.log('ERROR: could not receive ISS current location');
     res.status(500).json({ error: 'Failed to fetch data from API' });
@@ -26,6 +39,6 @@ app.get('/', async (req: Request, res: Response) => {
   console.log('Successfully received ISS current location');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(appConfig.port, () => {
+  console.log(`Server is running on http://localhost:${appConfig.port}`);
 });
